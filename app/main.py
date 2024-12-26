719061f6ad2ae043766826bbdae2128caab14413
AtharvaRaut2002 <atharvaraut200@gmail.com>
2024-12-26 18:08:44 +0530

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
+from fastapi import FastAPI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
 
 load_dotenv()
 app = FastAPI()
